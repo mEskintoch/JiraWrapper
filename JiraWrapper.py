e7c38f527fe7c106bcb88efef79dc3c3067ea391
mustafasamedeskin <meskin@wehkamp.nl>
2020-07-03 16:31:00 +0200

    private to public

--- JiraWrapper.py
+++ JiraWrapper.py
@@ -1,23 +1,39 @@
-import pandas as pd
-from jira.client import JIRA
-
-
 class JiraFacilitator:
 
-    def __init__(self, rsa_key, access_token, access_secret):
+    def __init__(self, rsa_key, access_token, access_secret, jira_server, consumer_key):
 
         self.rsa_key = rsa_key
         self.access_token = access_token
         self.access_secret = access_secret
-        self.JIRA_SERVER = 'https://jira.wehkampgroep.nl'
-        self.CONSUMER_KEY = 'OauthKey'
+        self.JIRA_SERVER = jira_server
+        self.CONSUMER_KEY = consumer_key
         self.jira_instance = self.connect()
 
+    #         self.project = project
+    #         self.project = project_key
+
     def all_issues(self, project):
+
+        """
+        Fetches all the issues from given jira project.
+
+        Args:
+          :param project: Jira Project (str)
+
+        Returns:
+          List of issues.
+        """
+
         return self.jira_instance.search_issues('project = "{}"'.format(project), maxResults=False)
 
     def connect(self):
 
+        """
+        Connects to Jira server.
+
+        Returns:
+          jira object.
+        """
 
         # Now you can use the access tokens with the JIRA client.
         jira = JIRA(options={'server': self.JIRA_SERVER}, oauth={
@@ -30,8 +46,28 @@ class JiraFacilitator:
 
     def post_issues(self, project, project_key, pdf, **kwargs):
 
-        #   for col in kwargs.keys():
-        #     assert col in df.columns, 'Column {} should be in the dataframe'.format(col)
+        """
+        Posts issues from a pandas dataframe to given jira project.
+
+        Args:
+          :param project: Jira Project (str)
+          :param project_key: Jira Project Key (str)
+          :param pdf: Pandas Dataframe
+          :param kwargs: field map dictionary.
+            Example:
+                  kwargs = {"col" :
+                            {
+                              'issue_field' : 'column name'
+                            },
+                            "lit" :
+                            {
+                              'issue_field' : 'string'
+                            }
+                           }
+
+        Returns:
+          None
+        """
 
         total = len(pdf)
 
@@ -39,13 +75,12 @@ class JiraFacilitator:
 
             post_dict_ = {"project": {"name": project, "key": project_key}}
 
-            for key, value in kwargs.items():
-
-                if not key.startswith('col_'):
-                    post_dict_[key] = value
-                else:
-                    key_ = key[4:]
-                    post_dict_[key_] = row[value]
+            if kwargs['lit']:
+                for k, v in kwargs['lit'].items():
+                    post_dict_[k] = v
+            if kwargs['col']:
+                for k, v in kwargs['col'].items():
+                    post_dict_[k] = row[v]
 
             response = self.jira_instance.create_issue(post_dict_)
             print('Alerts posted {} / {}. : {}'.format(index + 1, total, response))
@@ -54,68 +89,95 @@ class JiraFacilitator:
 
     def move_issues(self, pdf, issue_key_column, move_to: str, fields_map):
 
+        """
+        Function that executes issue transition within workflow. Takes input from
+          pandas dataframe in the same way that is applied in self.post_issues.
+
+        Args:
+          :param pdf: Pandas Dataframe
+          :param issue_key_column: Column name that contains issue key info.
+          :param move_to: workflow name to make transition to.
+          :param field_map: field map dictionary that contains issue info.
+            Example:
+                  kwargs = {"col" :
+                            {
+                              'issue_field' : 'column name'
+                            },
+                            "lit" :
+                            {
+                              'issue_field' : 'string'
+                            }
+                           }
+
+        Returns:
+          None
+        """
+
         assert issue_key_column in pdf.columns, f"Column {issue_key_column} is not in the dataframe!"
 
         for index, row in pdf.iterrows():
             fields = {}
-            for key, value in fields_map.items():
-                if not key.startswith('col_'):
-                    fields[key] = value
-                else:
-                    key_ = key[4:]
-                    fields[key_] = row[value]
+
+            if fields_map['lit']:
+                for k, v in fields_map['lit'].items():
+                    fields[k] = v
+            if fields_map['col']:
+                for k, v in fields_map['col'].items():
+                    fields[k] = row[v]
 
             response = self.jira_instance.transition_issue(row[issue_key_column], move_to, fields)
             print('Ticket {} is moved to {}'.format(row[issue_key_column], move_to))
 
         return None
 
-    def delete_issue(self, issue_str, delete_subtasks: bool = True):
-        return self.jira_instance.issue(issue_str).delete(deleteSubtasks=delete_subtasks)
+    def delete_issue(self, issue_key, delete_subtasks: bool = True):
 
-    def batch_delete_issues(self, issues_to_delete, issue_key_column: str, delete_subtasks: bool = True):
+        """
+        Deletes a single issue with given issue key.
 
-        assert issue_key_column in issues_to_delete.columns, 'alert_key is not in dataframe!'
+        Args:
+          :param issue_key: Issue Key (str)
 
-        if len(issues_to_delete) != 0:
-            print('Found issues to be deleted')
-            get_issues_list = issues_to_delete.select('alert_key').distinct().rdd.flatMap(list).collect()
-            for issue in get_issues_list:
+        Returns:
+          None
+        """
 
-                print('Deleting {}'.format(issue))
-                self.delete_issue(issue, delete_subtasks=delete_subtasks)
+        return self.jira_instance.issue(issue_key).delete(deleteSubtasks=delete_subtasks)
 
+    def delete_all_issues(self, project: str):
 
+        """
+        Deletes all the issues in a project.
 
-            print('Issues are deleted!')
-        else:
-            print('There are no issues to be deleted!')
+        Args:
+          :param project: jira project (str)
 
-        return None
+        Returns:
+          None
+        """
 
-    @staticmethod
-    def parse_issue(issue, field_mapping:dict):
+        print('Deleting all issues from project {}'.format(project))
+        for issue in self.all_issues(project):
+            self.delete_issue(issue, delete_subtasks=True)
 
-        raw = issue.raw
+    def jira_query(self, project: str, parse_issue_func):
 
-        clean_alert_dict = {}
-        clean_alert_dict['key'] = raw['key']
+        """
+        Get ticket information from given jira project (project) in the
+          given context (parse_issue_func)
 
-        for key, value in clean_alert_dict.items():
-            clean_alert_dict[key] = value
+        Args:
+          :param project: Jira Project (str)
+          :param parse_issue_funct: Python Function that applies on issue
 
-        return clean_alert_dict
+        Returns:
+          Spark Dataframe
+        """
 
-    @staticmethod
-    def parse_issues(self, issues):
-
-        clean_issue_list = []
-
-        for issue in issues:
-            clean_issue_list += [self.parse_issue(issue)]
-
-
-        issues_df = pd.DataFrame(clean_issue_list)
-
-        return issues_df
+        # get all issues
+        issues = self.all_issues(project=project)
+        if len(issues) == 0:
+            return None
+        issue_list = [parse_issue_func(issue) for issue in issues]
+        return spark.createDataFrame(Row(**d) for d in issue_list)
 
